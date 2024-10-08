995ac8784b58e0ef5f8dc2a5c5d4015e91e70725
Elkinssm <elkinsilvamanrique@gmail.com>
2024-10-07 23:33:55 -0500

    init calculator screen

---
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Pressable, Text, View} from 'react-native';
+import {globalStyles} from '../../config/theme/app-theme';
+
+export const CalculatorScreen = () => {
+  return (
+    <View style={globalStyles.calculatorContainer}>
+      <View style={globalStyles.secondContainer}>
+        <Text style={globalStyles.mainResult}>1500</Text>
+        <Text style={globalStyles.subResult}>15</Text>
+      </View>
+
+      <View style={globalStyles.row}>
+        <Pressable style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>1</Text>
+        </Pressable>
+
+      </View>
+    </View>
+  );
+};
